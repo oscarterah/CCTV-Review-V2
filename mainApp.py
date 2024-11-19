20dd8c3f6e2becba12636289bd71e45bfdc06569
oscarterah <oscarterah@gmail.com>
2024-11-19 03:25:53 +0300

    auto

--- mainApp.py
+++ mainApp.py
@@ -8,15 +8,27 @@ Created on Tue Nov 19 02:39:18 2024
 from kivymd.app import MDApp
 from VideoApp import MainWid
 from kivymd.uix.screenmanager import MDScreenManager
-
-
+from LiveStreamApp import LiveViewScreen
+from kivymd.uix.screen import Screen
 
 
 class ScreenManager(MDScreenManager):
     def __init__(self, **kwargs):
         super().__init__()
         self.mainwid_screen = MainWid()
+        self.livestream_screen = LiveViewScreen()
+        
+        
+        
+        wid = Screen(name="VideoApp")
+        wid.add_widget(self.mainwid_screen)
+        self.add_widget(wid)
+        
+        
+        
+        
         self.add_widget(self.mainwid_screen)
+        self.add_widget(self.livestream_screen)
         
 
 
